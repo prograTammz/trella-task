830874fda9990718b00987f87ee3f00222efc015
Ahmed Tamer <ahmedtamer1998@gmail.com>
2021-04-08 09:10:47 +0200

    added Data service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse, HttpHeaders, HttpParams } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Shipment } from './model/model';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  public shipments: Shipment[];
+  public location: Location;
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  public login(username: string) {
+    localStorage.setItem('email', username);
+    this.router.navigate(['listing']);
+  }
+
+  public getShipments(long: number = 30, lat: number = 30): Observable<HttpResponse<Shipment[]>> {
+    //return this.http.get<Shipment[]> (`https://case-api.trella.app/marketplace?lng=${long}&lat=${lat}`, {observe: 'response'});
+    return this.http.get<Shipment[]> (`assets/data.json`, {observe: 'response'});
+  }
 }
